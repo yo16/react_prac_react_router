25c0ff1db7112fe3b39a7e21a121f88b76930344
yo16 <xxyo1xx@gmail.com>
2024-02-07 10:42:19 +0900

    first commit

---
+++ react_router_app/src/App.js
@@ -0,0 +1,21 @@
+import { BrowserRouter, Link, Routes, Route } from "react-router-dom";
+import { TopPage, Page1, Page2 } from "./pages";
+
+function App() {
+    return (
+        <BrowserRouter>
+            <header>
+                <Link to="/">トップ</Link>｜<Link to="/page1">ページ１</Link>｜<Link to="/page2">ページ２</Link>
+            </header>
+            <main>
+                <Routes>
+                    <Route path="/" element={<TopPage />} />
+                    <Route path="/page1" element={<Page1 />} />
+                    <Route path="/page2" element={<Page2 />} />
+                </Routes>
+            </main>
+        </BrowserRouter>
+    );
+}
+
+export default App;
---
+++ react_router_app/src/pages.js
@@ -0,0 +1,4 @@
+export const TopPage = () => <>トップページ！</>;
+export const Page1 = () => <>Page1です</>;
+export const Page2 = () => <>Page2だよ</>;
+export const Page3 = () => <>Page3だ！</>;
